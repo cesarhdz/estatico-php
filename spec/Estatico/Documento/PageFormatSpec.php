72c16c69fdc4a90ee8647643a93a2ce02d738b26
Cesar Hernandez <mrshortcuts@gmail.com>
2014-03-15 15:16:27 -0600

    Documento/PageFormat supports html files

--- spec/Estatico/Documento/PageFormatSpec.php
+++ spec/Estatico/Documento/PageFormatSpec.php
@@ -10,5 +10,13 @@ class PageFormatSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Estatico\Documento\PageFormat');
+        $this->shouldImplement('Estatico\Documento\DocumentFormat');
+    }
+
+
+    function it_supports_html_files(){
+    	$this->shouldBeSupported('index.html');
+    	$this->shouldBeSupported('services/photo.html');
+    	$this->shouldBeSupported('services/web/desig.html');
     }
 }
--- src/Estatico/Documento/PageFormat.php
+++ src/Estatico/Documento/PageFormat.php
@@ -2,6 +2,26 @@
 
 namespace Estatico\Documento;
 
-class PageFormat
+class PageFormat implements DocumentFormat
 {
+
+	static $extensionWhiteList = array('html');
+
+    function isSupported($uri)
+    {
+        $file = new \SplFileInfo($uri);
+        $ext = $file->getExtension();
+
+        if(in_array($ext, self::$extensionWhiteList)) return true;
+    }
+
+
+    function setDir($dir){
+
+    }
+
+
+    function exists($filePath){
+
+    }
 }
