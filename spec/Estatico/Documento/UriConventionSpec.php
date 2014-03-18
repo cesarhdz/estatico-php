d6e3fff411c2729449338e69382c74e12ac7fd01
Cesar Hernandez <mrshortcuts@gmail.com>
2014-03-17 18:10:10 -0600

    UriConvention should have a default collection

--- spec/Estatico/Documento/UriConventionSpec.php
+++ spec/Estatico/Documento/UriConventionSpec.php
@@ -5,10 +5,19 @@ namespace spec\Estatico\Documento;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Estatico\Documento\UriConvention;
+
 class UriConventionSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Estatico\Documento\UriConvention');
     }
+
+    function it_should_have_a_default_collection(){
+    	//expect
+    	$defaultCollection = UriConvention::DEFAULT_COLLECTION;
+    	$this->getCollectionName()->shouldReturn($defaultCollection);
+    }
+
 }
--- src/Estatico/Documento/UriConvention.php
+++ src/Estatico/Documento/UriConvention.php
@@ -4,4 +4,17 @@ namespace Estatico\Documento;
 
 class UriConvention
 {
+
+	const DEFAULT_COLLECTION = 'root';
+
+
+
+    /**
+     * Get CollectionName
+     * @return String collectionName
+     */
+    public function getCollectionName()
+    {
+        return self::DEFAULT_COLLECTION;
+    }
 }
