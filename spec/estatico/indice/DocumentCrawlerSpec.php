b4d7087dc2dd3ca8e4dcb254079aba17679c7dc0
cesarhdz <mrshortcuts@gmail.com>
2014-03-27 10:25:39 -0600

    DocumentCrawler is constructed with a valid dir

--- spec/estatico/indice/DocumentCrawlerSpec.php
+++ spec/estatico/indice/DocumentCrawlerSpec.php
@@ -2,13 +2,24 @@
 
 namespace spec\estatico\indice;
 
+use org\bovigo\vfs\vfsStream as FS;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class DocumentCrawlerSpec extends ObjectBehavior
 {
+	private $homeDir;
+
+	function let(){
+		FS::setup('home');
+		$this->homeDir = FS::url('home');
+	}
+
     function it_is_initializable()
     {
+    	$this->beConstructedWith(FS::url('home'));
         $this->shouldHaveType('estatico\indice\DocumentCrawler');
+
+        $this->getDir()->shouldReturn($this->homeDir);
     }
 }
--- src/estatico/indice/DocumentCrawler.php
+++ src/estatico/indice/DocumentCrawler.php
@@ -4,4 +4,30 @@ namespace estatico\indice;
 
 class DocumentCrawler
 {
+	private $dir;
+
+    public function __construct($dir)
+    {
+        $this->setDir($dir);
+    }
+
+    /**
+     * 		
+     * @return String Dir that document is Crawling
+     */
+    public function getDir()
+    {
+        return $this->dir;
+    }
+
+    /**
+     * @param String $dir Only valid dirs are allowed
+     */
+	protected function setDir($dir){
+
+		var_dump($dir);
+		var_dump(is_dir($dir));
+
+		if(is_dir($dir)) $this->dir = $dir;
+	}
 }
